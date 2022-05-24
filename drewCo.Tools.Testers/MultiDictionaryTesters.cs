ffb389bece7ebf805b77087d07d40b44d3236584
drewCoSoftware <drew@august-harper.com>
2022-05-24 11:26:54 -0400

    - Added the JSONFile class.  This is like XMLFile, but for JSON content.  It is not available in the .net classic builds. - Added some missing test classes.

---
+++ drewCo.Tools.Testers/MultiDictionaryTesters.cs
@@ -0,0 +1,44 @@
+﻿using drewCo.Curations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace drewCo.Tools.Testers
+{
+  // ============================================================================================================================
+  [TestClass]
+  public class MultiDictionaryTesters
+  {
+
+    // --------------------------------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// This test case shows that the 'TryGetValue' function of MultiDictionary works as expected.
+    /// </summary>
+    [TestMethod]
+    public void CanUseTryGetValue()
+    {
+
+      const int KEY_1 = 100;
+      const string KEY_2 = "One Hundred";
+      const string TEST_VALUE = "I am a test value!";
+
+      var dict = new MultiDictionary<int, string, string>();
+      dict.Add(KEY_1, KEY_2, TEST_VALUE);
+
+      {
+        bool hasVal = dict.TryGetValue(KEY_1, KEY_2, out string checkVal);
+        Assert.IsTrue(hasVal);
+        Assert.AreEqual(checkVal, TEST_VALUE, "The test value does not match!");
+      }
+
+
+      // Show that it will come up empty for keys that don't exist.
+      {
+        const string KEY_3 = KEY_2 + "_notakey";
+        bool hasVal = dict.TryGetValue(KEY_1, KEY_3, out string checkVal);
+        Assert.IsFalse(hasVal);
+      }
+
+    }
+
+
+  }
+}
--- drewCo.Tools/JsonFile.cs
+++ drewCo.Tools/JsonFile.cs
@@ -1,11 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace drewCo.Tools
+
+using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+// ============================================================================================================================
+/// <summary>
+/// This is like XMLFile, but uses JSON.
+/// </summary>
+public class JsonFile<T>
+  where T : JsonFile<T>, new()
 {
-  class JsonFile
+  [JsonIgnore]
+  public string? FilePath { get; protected set; }
+
+  protected virtual void PreSave() { }
+  protected virtual void PostLoad() { }
+
+  // --------------------------------------------------------------------------------------------------------------------------
+  public void Save(string? filePath = null)
   {
+    FilePath = filePath != null ? filePath : null;
+    if (FilePath == null)
+    {
+      throw new InvalidOperationException("You must set a valid file path!");
+    }
+
+    PreSave();
+    string data = JsonSerializer.Serialize(this);
+    File.WriteAllText(FilePath, data);
+  }
+
+  // --------------------------------------------------------------------------------------------------------------------------
+  public static T Load(string filePath_)
+  {
+    string data = File.ReadAllText(filePath_);
+    var serializerOps = new JsonSerializerOptions()
+    {
+      AllowTrailingCommas = true,
+    };
+    T res = JsonSerializer.Deserialize<T>(data, serializerOps);
+    if (res == null)
+    {
+      throw new InvalidOperationException("Could not deserialize type {typeof(T)} from json data!");
+    }
+    res.FilePath = filePath_;
+    res.PostLoad();
+    return res;
   }
 }
