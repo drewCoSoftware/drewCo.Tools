102453750ff134907cd38da9d9ba5e6389360c0b
Andrew A. Ritz <drew@august-harper.com>
2025-07-03 22:20:39 -0400

    - Added a test case for a bug with the sequential file names that was detected during development...

---
+++ drewCo.Tools.Core.Testers/FileToolTesters.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drewCo.Tools.Core.Testers
+{
+
+  public class FileToolTesters
+  {
+    
+  [Test]
+    public void CanComputeSequentialFileNameFromExistingName()
+    {
+
+      string testDir = FileTools.GetLocalDir("test-sequential");
+      FileTools.EmptyDirectory(testDir);
+      FileTools.CreateDirectory(testDir);
+
+      string testName = Path.Combine(testDir, "my-file.json");
+
+      string output = FileTools.GetSequentialFileName(testName);
+
+      // The file names should be the same after the first call!.
+      Assert.AreEqual(testDir, testName);
+    }
+
+  }
+}
